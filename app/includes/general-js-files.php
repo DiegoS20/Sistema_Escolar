cf27860a073e1747d83d2f3a938cdf64a9f7b45f
Diego José Saravia Zaldaña <saravia__2000@hotmail.com>
2020-03-14 15:05:09 -0500

    Initial design of login page done.

--- app/includes/general-js-files.php
+++ app/includes/general-js-files.php
@@ -1,1 +1,4 @@
-<script src="<?php echo $conexion->get_path_from_origin('app/js/bootstrap.min.js') ?>"></script>
+<script src="<?php echo $conexion->get_relative_path('app/js/jquery.min.js', $dir) ?>"></script>
+<script src="<?php echo $conexion->get_relative_path('app/js/popper.min.js', $dir) ?>"></script>
+<script src="<?php echo $conexion->get_relative_path('app/js/bootstrap.min.js', $dir) ?>"></script>
+<script src="<?php echo $conexion->get_relative_path('app/js/sweetalert2.min.js', $dir) ?>"></script>
--- login.php
+++ login.php
@@ -1,13 +1,28 @@
 <?php require_once('app/models/Conexion.php'); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Sistema escolar</title>
+    <link rel="stylesheet" href="app/css/main.css">
+    <link rel="stylesheet" href="app/css/login.css">
 </head>
+
 <body>
-    
-    <?php include 'app/includes/general-js-files.php'; ?>
+    <main>
+        <div class="bg"></div>
+        <div class="container">
+            <div class="login">
+
+            </div>
+        </div>
+    </main>
+    <?php
+    $dir = __DIR__;
+    include 'app/includes/general-js-files.php';
+    ?>
 </body>
+
 </html>
