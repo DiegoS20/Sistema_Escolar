36caf6a1f75e4d5d72140cc5159bb70de95c59f1
Diego José Saravia Zaldaña <saravia__2000@hotmail.com>
2020-03-14 15:39:24 -0500

    Adding Bootstrap Material Design

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
     <title>Login | Sistema escolar</title>
     <link rel="stylesheet" href="app/css/main.css">
     <link rel="stylesheet" href="app/css/login.css">
+    <link rel="stylesheet" href="app/css/bootstrapmaterial.min.css">
 </head>
 
 <body>
@@ -15,14 +16,23 @@
         <div class="bg"></div>
         <div class="container">
             <div class="login">
+                <div class="head-icon"><i class="fas fa-user-circle"></i></div>
+                <form action="">
 
+                </form>
             </div>
         </div>
     </main>
-    <?php
-    $dir = __DIR__;
-    include 'app/includes/general-js-files.php';
-    ?>
+    <script src="app/js/jquery.min.js"></script>
+    <script src="app/js/popper.min.js"></script>
+    <script src="app/js/bootstrapmaterial.min.js"></script>
+    <script src="app/js/sweetalert.min.js"></script>
+    <script src="app/js/fontawesome.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('body').bootstrapMaterialDesign();
+        });
+    </script>
 </body>
 
 </html>
