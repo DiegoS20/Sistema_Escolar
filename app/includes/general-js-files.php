69c105d6ab15cd60cf01d9ef23bdb32fdf8e2acf
Diego José Saravia Zaldaña <saravia__2000@hotmail.com>
2020-03-14 15:16:01 -0500

    Adding Font Awesome to the proyect

--- app/includes/general-js-files.php
+++ app/includes/general-js-files.php
@@ -2,3 +2,4 @@
 <script src="<?php echo $conexion->get_relative_path('app/js/popper.min.js', $dir) ?>"></script>
 <script src="<?php echo $conexion->get_relative_path('app/js/bootstrap.min.js', $dir) ?>"></script>
 <script src="<?php echo $conexion->get_relative_path('app/js/sweetalert2.min.js', $dir) ?>"></script>
+<script src="<?php echo $conexion->get_relative_path('app/js/fontawesome.min.js', $dir) ?>"></script>
